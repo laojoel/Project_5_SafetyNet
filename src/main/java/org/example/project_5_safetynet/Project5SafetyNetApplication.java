b79b855db5b61e66c57506ce7c355241011cd4e7
Joel LAO <joel.lao@axa.fr>
2024-09-05 15:48:14 +0200

    Sauvegarde des Logs dans un fichier text (log.txt) situé dans le folder "Ressources".

--- src/main/java/org/example/project_5_safetynet/Project5SafetyNetApplication.java
+++ src/main/java/org/example/project_5_safetynet/Project5SafetyNetApplication.java
@@ -1,19 +1,24 @@
 package org.example.project_5_safetynet;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.example.project_5_safetynet.DAO.DataDAO;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 import java.io.IOException;
 
 @SpringBootApplication
 public class Project5SafetyNetApplication {
-    public static final Logger logger = LogManager.getLogger("Project5SafetyNetApplication");
+    public static final Logger logger = LoggerFactory.getLogger(Project5SafetyNetApplication.class);
 
     public static void main(String[] args) throws IOException {
         logger.info("Initializing SafetyNet System");
+
+        System.out.println("Log4j configuration file location: " + System.getProperty("log4j2.configurationFile"));
+
         SpringApplication.run(Project5SafetyNetApplication.class, args);
         DataDAO.initWithFilePath("src/main/resources/data.json");
     }
